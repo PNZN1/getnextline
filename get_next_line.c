96c6c9ae71f40e3ad646fab475ea97dc2b39f1d3
Paul Niezen <pniezen@f0r5s15.codam.nl>
2022-05-15 08:48:01 +0200

    first commit

---
+++ get_next_line.c
@@ -0,0 +1,117 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   get_next_line.c                                    :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: pniezen <pniezen@student.codam.nl>           +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2021/11/19 15:58:09 by pniezen       #+#    #+#                 */
+/*   Updated: 2021/12/08 11:16:15 by pniezen       ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+static void	set_zero(char *buffer)
+{
+	int	i;
+
+	i = 0;
+	while (i < BUFFER_SIZE)
+	{
+		buffer[i] = '\0';
+		i++;
+	}
+}
+
+static char	*get_one_line(char *src)
+{
+	char	*tmp;
+	int		i;
+
+	if (!src)
+		return (free_null(src));
+	tmp = ft_calloc(ft_strlen(src) + 1, sizeof(char));
+	if (!tmp)
+	{
+		free(src);
+		return (NULL);
+	}
+	i = 0;
+	while (src[i])
+	{
+		tmp[i] = src[i];
+		if (src[i] == '\n')
+			break ;
+		i++;
+	}
+	free(src);
+	return (tmp);
+}
+
+static char	*read_to_return(int fd, char *return_line)
+{
+	ssize_t	rd_fd;
+	char	*buffer;
+
+	rd_fd = 1;
+	buffer = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+	if (!buffer)
+	{
+		free(buffer);
+		return (free_null(return_line));
+	}
+	while (!(check_new_line(return_line)) && rd_fd > 0)
+	{
+		rd_fd = read(fd, buffer, BUFFER_SIZE);
+		if (rd_fd <= 0 && return_line[0] == '\0')
+		{
+			free(return_line);
+			return (free_null(buffer));
+		}
+		return_line = realloc_and_join(return_line, buffer);
+		set_zero(buffer);
+	}
+	free(buffer);
+	return (return_line);
+}
+
+static void	set_storage(char *storage, char *return_line)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (return_line[i] != '\n' && return_line[i])
+		i++;
+	if (return_line[i] != '\0')
+		i++;
+	j = 0;
+	while (return_line[i])
+	{
+		storage[j] = return_line[i];
+		i++;
+		j++;
+	}
+	storage[j] = '\0';
+}
+
+char	*get_next_line(int fd)
+{
+	static char	storage[OPEN_MAX][BUFFER_SIZE + 1];
+	char		*return_line;
+
+	if (fd < 0 || fd > OPEN_MAX || BUFFER_SIZE <= 0)
+		return (NULL);
+	return_line = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+	if (!return_line)
+		return (NULL);
+	return_line = realloc_and_join(return_line, storage[fd]);
+	if (!return_line)
+		return (NULL);
+	return_line = read_to_return(fd, return_line);
+	if (return_line)
+		set_storage(storage[fd], return_line);
+	return_line = get_one_line(return_line);
+	return (return_line);
+}
---
+++ get_next_line.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   get_next_line.h                                    :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: pniezen <pniezen@student.codam.nl>           +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2021/11/19 15:55:14 by pniezen       #+#    #+#                 */
+/*   Updated: 2021/12/08 11:12:10 by pniezen       ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <unistd.h>
+# include <stdlib.h>
+# include <limits.h>
+
+char	*get_next_line(int fd);
+char	*free_null(char *str);
+void	*ft_calloc(size_t count, size_t size);
+int		check_new_line(char *storage);
+char	*realloc_and_join(char *storage, char *buff);
+size_t	ft_strlen(const char *s);
+
+#endif
